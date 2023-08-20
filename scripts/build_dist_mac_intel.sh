f61212444acb52da8439ef6e7becb0741875c39a
Florian Fontan <dev@florian-fontan.fr>
2023-08-20 14:14:45 +0200

    Update scripts

--- scripts/build_dist_mac_intel.sh
+++ scripts/build_dist_mac_intel.sh
@@ -18,15 +18,15 @@ mkdir "${TARGET_DIR}/lib"
 cp "./scripts/run.mac.sh" "${TARGET_DIR}/run.sh"
 # Copy data.
 cp ./data/example/*.csv "${TARGET_DIR}/data/"
+# Build and copy executable.
+bazel build -- //...
+cp "./bazel-bin/timetablingsolver/main" "${TARGET_DIR}/bin/TimetablingSolver"
+chmod 755 "${TARGET_DIR}/bin/TimetablingSolver"
 # Copy lib.
 cp ./bazel-timetablingsolver/external/cbc_darwin/lib/*.dylib "${TARGET_DIR}/lib/"
 cp ./bazel-timetablingsolver/external/clp_darwin/lib/*.dylib "${TARGET_DIR}/lib/"
 cp ./bazel-timetablingsolver/external/coinutils_darwin/lib/*.dylib "${TARGET_DIR}/lib/"
 cp ./bazel-timetablingsolver/external/osi_darwin/lib/*.dylib "${TARGET_DIR}/lib/"
-# Build and copy executable.
-bazel build -- //...
-cp "./bazel-bin/timetablingsolver/main" "${TARGET_DIR}/bin/TimetablingSolver"
-chmod 755 "${TARGET_DIR}/bin/TimetablingSolver"
 # Copy visualizer.
 pyinstaller -F "./scripts/visualizer.py"
 mv "./dist/visualizer" "${TARGET_DIR}/bin/visualizer"
